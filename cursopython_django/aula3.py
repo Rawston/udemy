52897f5f256a5c962d0de6472dcb1b6dea0a6c12
Rawston <houston_cam@msn.com>
2024-09-17 23:53:09 -0300

    reorganizando

---
+++ cursopython_django/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Rawston Marinho')
+print(1, 'Rawston "Marinho"')
+
+# Aspas duplas
+print("Rawston Marinho")
+print(2, "Rawston 'Marinho'")
+
+# Escape
+print("Rawston \"Marinho\"")
+
+# r
+print(r"Rawston \"Marinho\"")
